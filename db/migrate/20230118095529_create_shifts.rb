7755f933661abcc0b15e63f25fcb54a28e6ef64d
shuto012 <rinGo0120X@gmail.com>
2023-01-18 18:58:53 +0900

    マイグレーションファイル編集

--- db/migrate/20230118095529_create_shifts.rb
+++ db/migrate/20230118095529_create_shifts.rb
@@ -2,6 +2,10 @@ class CreateShifts < ActiveRecord::Migration[6.0]
   def change
     create_table :shifts do |t|
 
+      t.datetime :start_time,             null: false
+      t.datetime :finish_time,            null: false
+      t.text :comment
+
       t.timestamps
     end
   end
