7ff05c41bb18930bbda31d037623c6250a9e1b9d
shuto012 <rinGo0120X@gmail.com>
2023-01-19 22:15:51 +0900

    コントローラ設定

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,5 +1,17 @@
 class ShiftsController < ApplicationController
   
   def new
+    @shift = Shift.new
   end
+
+  def create
+    @shift = Shift.new(shift_params)
+  end
+
+  private
+  
+  def shift_params
+    params.require(:shift).permit(:start_time, :finish_time, :comment)
+  end
+
 end
