852f8f93f3a9de8721639e383a78f8bafce42fa9
shuto012 <rinGo0120X@gmail.com>
2023-01-19 23:28:48 +0900

    コントローラ設定

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -6,13 +6,14 @@ class ShiftsController < ApplicationController
 
   def create
     @shift = Shift.new(shift_params)
+    @shift.save
     redirect_to new_shift_path
   end
 
   private
   
   def shift_params
-    params.require(:shift).permit(:start_time, :finish_time, :comment)
+    params.require(:shift).permit(:start_time, :finish_time, :comment).merge(user_id: current_user.id)
   end
 
 end
