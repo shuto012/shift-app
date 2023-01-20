a2b6aa032b7802e368b864040cc368e92d617a79
shuto012 <rinGo0120X@gmail.com>
2023-01-20 20:47:25 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'shifts/new'
+  root to: "shifts#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shifts
