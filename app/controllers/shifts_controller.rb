d440e30174a429a135e55ed12e462b632df09955
shuto012 <rinGo0120X@gmail.com>
2023-02-05 15:52:37 +0900

    ビュー変更

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,11 +2,18 @@ class ShiftsController < ApplicationController
   
   def index
     @shift = Shift.new
-    
+    @shifts = Shift.all
+    get_days
+  end
+
+  def new
+    @shift = Shift.new
+    @shifts = Shift.all
+    get_days
   end
 
   def create
-    Shift.create(shift_params)
+    @shift = Shift.create(shift_params)
     redirect_to root_path
   end
 
@@ -21,4 +28,18 @@ class ShiftsController < ApplicationController
   def shift_params
     params.require(:shift).permit(:date, :start_time, :finish_time, :comment).merge(user_id: current_user.id)
   end
+
+  def get_days
+    @today = Date.today
+    @first_date = @today.beginning_of_month
+    @last_date = @today.end_of_month
+    last_day = @last_date.day
+
+    @days =[]
+
+    last_day.times do |i|
+      @days = @days.push(i + 1)
+    end
+  end
+
 end
