a391345a8324eea4edecd6e3416314f03ad13ee7
shuto012 <rinGo0120X@gmail.com>
2023-02-13 21:34:10 +0900

    マイグレーションファイル編集

--- db/migrate/20230114195733_devise_create_users.rb
+++ db/migrate/20230114195733_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false
       t.integer :number,            null: false
+      t.integer :editor,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_095529) do
-
-  create_table "shifts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.date "date", null: false
-    t.time "start_time", null: false
-    t.time "finish_time", null: false
-    t.text "comment"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_shifts_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2023_01_14_195733) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,5 +26,4 @@ ActiveRecord::Schema.define(version: 2023_01_18_095529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "shifts", "users"
 end
