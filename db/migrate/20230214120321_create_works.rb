68fa70fc3ecbaa996c04d19309308c2a7964d5fc
shuto012 <rinGo0120X@gmail.com>
2023-02-14 21:04:27 +0900

    work マイグレーションファイル設定

--- db/migrate/20230214120321_create_works.rb
+++ db/migrate/20230214120321_create_works.rb
@@ -1,7 +1,10 @@
 class CreateWorks < ActiveRecord::Migration[6.0]
   def change
     create_table :works do |t|
-
+      t.date :date,                   null: false
+      t.time :start_time,             null: false
+      t.time :finish_time,            null: false
+      t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
   end
