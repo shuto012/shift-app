36379c13366156a5a58650bef57450ad82093398
shuto012 <rinGo0120X@gmail.com>
2023-02-14 21:04:57 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_095529) do
+ActiveRecord::Schema.define(version: 2023_02_14_120321) do
 
   create_table "shifts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date", null: false
@@ -38,5 +38,16 @@ ActiveRecord::Schema.define(version: 2023_01_18_095529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "works", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date", null: false
+    t.time "start_time", null: false
+    t.time "finish_time", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_works_on_user_id"
+  end
+
   add_foreign_key "shifts", "users"
+  add_foreign_key "works", "users"
 end
