2a139ebb4814915c32653a9d71497327413ea009
shuto012 <rinGo0120X@gmail.com>
2023-02-14 20:42:44 +0900

    devise　ストロングパラメータ editor追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :number, :editor_id])
   end
 end
